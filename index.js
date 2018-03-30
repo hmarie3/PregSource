96637ce1bfa5daa8244f6654846dea8928abbe56
heather <hmbee3@gmail.com>
2018-03-29 22:27:33 -0500

    heroku troubleshooting

--- index.js
+++ index.js
@@ -5,31 +5,31 @@ const { URL } = require('url');
 var bodyParser = require('body-parser');
 var db_url = process.env.DATABASE_URL;
 
-if(typeof db_url !== 'undefined' && db_url !== null) {
-   //var scheme = db_url.protocol.substr(0, db_url.protocol.length - 1);
-   var user = db_url.auth.substr(0, db_url.auth.indexOf(':'));
-   var pass = db_url.auth.substr(db_url.auth.indexOf(':') + 1, db_url.auth.length);
-   var host = db_url.host.substr(0, db_url.host.indexOf(':'));
-   var port = db_url.host.substr(db_url.host.indexOf(':') + 1, db_url.host.length);
-   var db = db_url.path.substr(db_url.path.indexOf('/') + 1, db_url.path.length);
-} else {
-   var scheme = "postgres";
-   var host = "localhost";
-   var user = "heather";
-   var pass = "33433233";
-   var port = "5432";
-   var db = "pregsource";
-   
-}
-const client = new Client({
-  host: host,
-  user: user,
-  database: db,
-  password: pass,
-  port: port
-});
+// if(typeof db_url !== 'undefined' && db_url !== null) {
+//    var scheme = db_url.protocol.substr(0, db_url.protocol.length - 1);
+//    var user = db_url.auth.substr(0, db_url.auth.indexOf(':'));
+//    var pass = db_url.auth.substr(db_url.auth.indexOf(':') + 1, db_url.auth.length);
+//    var host = db_url.host.substr(0, db_url.host.indexOf(':'));
+//    var port = db_url.host.substr(db_url.host.indexOf(':') + 1, db_url.host.length);
+//    var db = db_url.path.substr(db_url.path.indexOf('/') + 1, db_url.path.length);
+// } else {
+//    var scheme = "postgres";
+//    var host = "localhost";
+//    var user = "heather";
+//    var pass = "33433233";
+//    var port = "5432";
+//    var db = "pregsource";   
+// }
 
-client.connect();
+// const client = new Client({
+//   host: host,
+//   user: user,
+//   database: db,
+//   password: pass,
+//   port: port
+// });
+
+client.connect(db_url);
 
 app.get('/', function(req, res) {
    res.render('home');
