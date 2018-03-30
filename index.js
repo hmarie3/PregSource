48afedaa319e5d89043295c8d7df398898d41243
heather <hmbee3@gmail.com>
2018-03-29 23:11:57 -0500

    trying example code for heroku database

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 var pg = require('pg');
 
-app.get('/db', function (request, response) {
+app.get('/firsttri', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM resources WHERE firsttri = "yes"', function(err, result) {
       done();
