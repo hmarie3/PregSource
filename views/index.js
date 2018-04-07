5f4c9a966d6e5d52b0d422c56c2b83eb44411aba
heather <hmbee3@gmail.com>
2018-04-06 21:39:08 -0500

    clean up

--- views/index.js
+++ views/index.js
@@ -3,23 +3,23 @@ var app = express();
 const { Client } = require('pg');
 const url = require('url');
 var bodyParser = require('body-parser');
-var db_url = url.parse(process.env.DATABASE_URL) /*|| 'postgres=//heather:33433233@localhost:5432/pregsource'*/;
+var db_url = url.parse(process.env.DATABASE_URL);
 
-if(typeof db_url !== 'undefined' && db_url !== null) {
+//if(typeof db_url !== 'undefined' && db_url !== null) {
    var scheme = db_url.protocol.substr(0, db_url.protocol.length - 1);
    var user = db_url.auth.substr(0, db_url.auth.indexOf(':'));
    var pass = db_url.auth.substr(db_url.auth.indexOf(':') + 1, db_url.auth.length);
    var host = db_url.host.substr(0, db_url.host.indexOf(':'));
    var port = db_url.host.substr(db_url.host.indexOf(':') + 1, db_url.host.length);
    var db = db_url.path.substr(db_url.path.indexOf('/') + 1, db_url.path.length);
-} else {
-   var scheme = "postgres";
-   var host = "localhost";
-   var user = "heather";
-   var pass = "33433233";
-   var port = "5000";
-   var db = "pregsource";   
-}
+// } else {
+//    var scheme = "postgres";
+//    var host = "localhost";
+//    var user = "heather";
+//    var pass = "33433233";
+//    var port = "5000";
+//    var db = "pregsource";   
+// }
 
 const client = new Client({
   host: host,
@@ -35,13 +35,11 @@ app.get('/', function(req, res) {
    res.render('home');
 });
 
-//app.use(express.static(_dirname + "/views"));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/firsttri', function(req,res) {
    client.query("SELECT * FROM resources WHERE firsttri = 'yes'", function(err, result) {
-      //console.log(err, result);
       res.render("firsttri", {
          EJSresult: result
       });
@@ -50,7 +48,6 @@ app.get('/firsttri', function(req,res) {
 
 app.get('/secondtri', function(req,res) {
    client.query("SELECT * FROM resources WHERE secondtri = 'yes'", function(err, result) {
-      //console.log(err, result);
       res.render("secondtri", {
          EJSresult: result
       });
@@ -59,7 +56,6 @@ app.get('/secondtri', function(req,res) {
 
 app.get('/thirdtri', function(req,res) {
    client.query("SELECT * FROM resources WHERE thirdtri = 'yes'", function(err, result) {
-      //console.log(err, result);
       res.render("thirdtri", {
          EJSresult: result
       });
@@ -68,10 +64,8 @@ app.get('/thirdtri', function(req,res) {
 
 app.get('/postpartum', function(req,res) {
    client.query("SELECT * FROM resources WHERE postpartum = 'yes'", function(err, result) {
-      //console.log(err, result);
-      //console.log(client.query("SELECT * FROM resources WHERE postpartum = 'yes'"));
       res.render("postpartum", {
-         EJSresult: result
+        EJSresult: result
       });
    });
 });
